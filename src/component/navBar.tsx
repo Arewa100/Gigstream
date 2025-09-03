6410c49b6c1912dc5633a67eefae786e308ba487
Arewa100 <olasorexway@gmail.com>
2025-09-03 15:36:28 +0100

    new update on the frontend

--- src/component/navBar.tsx
+++ src/component/navBar.tsx
@@ -2,14 +2,19 @@ const NavBar = () => {
     return(
         <>
             <div className="fixed top-[68px] left-0 right-0 z-50">
-                <div className="w-full max-w-[1440px] h-[102px] mx-auto flex items-center justify-between bg-[#645CF6]/60">
-                   <div className="h-full w-[533.2px] ">
-                    <img 
+                <div className="w-full max-w-[1440px] h-[102px] mx-auto flex items-center justify-between bg-[#645CF6]/60 relative">
+         
+                    <div className="absolute inset-0 w-full h-full bg-red-500 pointer-events-none">
+                        current area
+                    </div>
+                    
+                    <div className="h-full w-[533.2px] relative z-10">
+                        <img 
                             src="src\assets\gigLogo.svg" 
                             alt="Header image"
                             className="w-full h-full max-w-[250px] md:max-w-full max-h-[200px] md:max-h-full object-contain opacity-20"
                         />  
-                   </div>
+                    </div>
                 </div>
             </div>
         </>
